a347ccc86d1d6756c80f6b92bf01f9d6f73aeeeb
Michael Pacheco <git@michaelpacheco.org>
2023-02-21 07:39:15 -0800

    WIP: Add struct representations for the structure defined in the DNS header.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,122 @@
 // const
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+use std::ops::Index;
 
 struct DNSRequest {
     header: DNSHeader,
-    question:DNSQuestion,
+    question: Question,
 }
 
 // impl DNSRequest {
-//     fn new(byte_stream: Option<&[u8]>) -> Self {
-//         Sel
+//     fn new(packet: Option<&[u8]>) -> Self {
+//         if let Some(packet) = packet {
+//
+//         } else {
+//
+//         }
 //     }
 // }
 
 struct DNSResponse {
-
+    header: DNSHeader,
+    response: Response,
 }
 
-struct DNSQuestion {
+struct Question {
     qname: [u8; 2],
     qtype: [u8; 2],
     // q
 }
 
+struct Response {}
 
 struct DNSHeader {
     id: [u8; 2],
     // might make this another struct
-    flags: [u8; 2],
+    flags: Flags,
     qdcount: [u8; 2],
     ancount: [u8; 2],
     arcount: [u8; 2],
 }
+
+enum QRBit {
+    Query,
+    Response,
+}
+
+struct Flags {
+    dns_type: QRBit,
+    opcode: Opcode,
+    truncated: bool,
+    recursion: bool,
+    // reserved <- bit
+    ad_bit: bool,
+    non_auth: bool,
+}
+
+#[derive(FromPrimitive)]
+enum Opcode {
+    // 0
+    Query = 0,
+    // 1
+    IQuery = 1,
+    // 2
+    Status = 2,
+    // bits 3, 6-15, we do not want to use this anyways
+    // TODO: assign this? probably wont use complex types. This means that the addition  of
+    //   another crate, which means longer compile times.
+    // NotAssigned,
+    // 4
+    Notify = 4,
+    // 5
+    Update = 5,
+}
+
+impl Flags {
+    // TODO: Custom Error (possibly use anyhow?), or leave as string?
+    fn new(flags: &u8) -> Result<Self, String> {
+        let up_opcode = Flags::format_opcode(flags);
+        //Iterates through possible bit values that Opcode can be,
+        let opcode = (0..15)
+            .into_iter()
+            .rev()
+            .find(|&val| bitflags_check(val, &up_opcode))
+            .and_then(Opcode::from_u8)
+            // TODO: Decide if multiple conditions are necessary, or if an optional is better.
+            .ok_or_else(|| format!("DNS_TYPE: INVALID TYPE: {up_opcode}, AS BITS: {up_opcode:08b}"))?;
+        let dns_type = if bitflags_check(0, flags) {
+            QRBit::Response
+        } else {
+            QRBit::Query
+        };
+        Ok(Self {
+            dns_type,
+            opcode,
+            truncated: false,
+            recursion: false,
+            ad_bit: false,
+            non_auth: false,
+        })
+    }
+    // truncates the first bit and the last 3 bits, this way we only have the opcode bits
+    fn format_opcode(flag_bit: &u8) -> u8 {
+        let flag_bit = flag_bit >> 1;
+        let flag_bit = flag_bit << 3;
+        flag_bit >> 3
+    }
+
+}
+fn bitflags_check(bit: u8, bits: &u8) -> bool {
+    (bits & (1 << bit)) == 1
+}
+//           0     Query                              [RFC1035] ?
+//           1     IQuery (Inverse Query, Obsolete)   [RFC3425] Do not need to implement
+//           2     Status                             [RFC1035] ?
+//           3     available for assignment                     Do not need to implement
+//           4     Notify                             [RFC1996] ?
+//           5     Update                             [RFC2136] ?
+//          6-15   available for assignment
 // RAW [
 // DA, C5,
 // 01, 20,
@@ -54,3 +141,14 @@ struct DNSHeader {
 //   type: two bytes, 00 01 type A                                  00 01
 //   class: IN                                                      00 01
 
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn bits_test(){
+        let bits = 1;
+        let bit = 0;
+        let x: Option<&str> = None;
+        assert!(bitflags_check(bit, &bits), "Function does not work");
+    }
+}
